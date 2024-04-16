f60adc5fa18d49ef8d23e3f7d8951457edececf2
Maurino666 <maurilio.larocca@gmail.com>
2024-04-16 18:32:21 +0200

    CI HO FICCATO IL DATABASE

---
+++ DB/Database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `Utente`(
+    `username` VARCHAR(255) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
+    `cognome` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `n_telefono` CHAR(10) NOT NULL,
+    `password` CHAR(256) NOT NULL,
+    PRIMARY KEY(`username`)
+);
+CREATE TABLE `Prodotto`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nome` VARCHAR(255) NOT NULL,
+    `descrizione` VARCHAR(255) NOT NULL,
+    `prezzo` DOUBLE NOT NULL,
+    `fascia_iva` DOUBLE NOT NULL,
+    `dimensioni` CHAR(10) NOT NULL,
+    `disponibilita` SMALLINT NOT NULL,
+    `categoria` VARCHAR(255) NOT NULL,
+    `colore` VARCHAR(255) NOT NULL,
+    `immagine` BLOB
+);
