e90ec0cd82d11422cd9d2272e198a87f95a6a690
GuardianMont <86737951+GuardianMont@users.noreply.github.com>
2024-04-17 12:45:24 +0200

    Update Database.sql

--- DB/Database.sql
+++ DB/Database.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Utente`(
     `cognome` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `n_telefono` CHAR(10) NOT NULL,
-    `password` CHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     PRIMARY KEY(`username`)
 );
 CREATE TABLE `Prodotto`(
